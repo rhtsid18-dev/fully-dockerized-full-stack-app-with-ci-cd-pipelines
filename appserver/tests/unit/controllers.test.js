201d491a0847d2d8136702dc582b73b529b5c8d2
rhtsid18-dev <rhtsid18.dev@gmail.com>
2022-11-12 22:20:59 +0530

    Added dummy unit test to apply in the CI pipeline in the future CI CD dev work.

---
+++ appserver/tests/unit/controllers.test.js
@@ -0,0 +1,9 @@
+describe('Controllers Test Suite', () => {
+	it('Should do something', () => {
+		expect(true).toBe(true);
+	})
+
+	it('should do something else too', () => {
+		expect(true).toBe(true);
+	});
+});
